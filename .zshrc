13cf0571682a49a39c5c6c149280c5dcea375d33
br-netw <vanmain.22@gmail.com>
2023-03-27 00:52:46 +0300

    Added dotfiles for sway and waybar

---
+++ .zshrc
@@ -0,0 +1,23 @@
+# setting WM as sway
+export XDG_CURRENT_DESKTOP="sway"
+
+# prompt
+autoload -U colors && colors
+PS1='%B%F{yellow}%n%F{magenta} @ %F{green}%d
+->%f%b '
+
+# autostart sway on tty1
+if [ -z "${WAYLAND_DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+  exec sway
+fi
+
+# history
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=1000
+SAVEHIST=1000
+
+# aliases
+alias sudo="doas"
+alias vim="nvim"
+alias ls="ls --color=always"
+alias screenlock="swaylock -c 282828"
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,16 +1,10 @@
 # symbol composition
 setxkbmap -option compose:menu
-# Set up language toggle
-setxkbmap -model pc104 -layout us,ru -variant ,, -option grp:alt_shift_toggle
 
-# Screen temp and brightness
-#xrandr --output LVDS-1 --brightness 0.8
-redshift -P -O 4500K
+# waybar
+killall -q waybar
+waybar
 
-# Picom for transparency
-# Actually, turned it off. 
-# Looks unbearable with context menus
-#picom --daemon
-
-# polybar
-polybar
+# tray applets
+blueman-applet
+nm-applet --indicator
